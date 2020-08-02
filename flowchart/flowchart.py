8619038266a07c35508822450a0d11d236d4121f
root <root@SC-202003270754.localdomain>
2020-08-02 18:00:33 +0800

    up

--- flowchart/flowchart.py
+++ flowchart/flowchart.py
@@ -19,20 +19,33 @@ def catch_error(value=None):
 
 
 class fnode(object):
-    def __init__(self, name=None, left=None, right=None, down=None):
+    def __init__(self,
+                 name  = None,
+                 left  = None,
+                 right = None,
+                 down  = None,
+                 kw    = False):
         self.name = name
         self.left = left
         self.right = right
         self.down = down
+        self.kw = kw
 
 
 class compile(object):
     def __init__(self, fk):
         self.content = None
         self.del_char = lambda x, c: re.sub(r" *\%s *" % str(c), c, x)
+        self.stack = list()
+        self.nodelist = list()
         with open(fk, 'r') as fd:
             self.content = fd.read()
         self._process_fk()
+        self._parse_fk()
+
+    def _get_kw(self, fstr):
+        rest = re.search(r'^(while|if|else if|else)$', fstr, re.IGNORECASE)
+        return rest.group() if rest else None
 
     @catch_error()
     def _process_fk(self):
@@ -41,6 +54,30 @@ class compile(object):
         for c in ['(', ')', '{', '}']:
             self.content = self.del_char(self.content, c)
 
+    @catch_error()
+    def _parse_fk(self):
+        segs = self.content.split("{") or []
+        self.fk_segs = list()
+        for fk in segs:
+            self.fk_segs.extend(fk.split(";"))
+        print(self.fk_segs)
+        
+    def _parse_fk_sg_node(self, nstr):
+        node = fnode(name=nstr)
+        if self.is_kw(nstr, "while"):
+            node.kw = "while"
+            self.stack.append(node)
+        elif self.is_kw(nstr, "else if"):
+            node.kw = "else if"
+        elif self.is_kw(nstr, "if"):
+            node.kw = "else"
+        elif self.is_kw(nstr, "else"):
+            node.kw = "else"
+
+        self.nodelist.append(node)
+        return node
+
+        
 
 if '__main__' == __name__:
     compile("./fct.fk")
